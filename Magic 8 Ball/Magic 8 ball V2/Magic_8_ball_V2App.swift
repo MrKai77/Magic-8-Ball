2df7e476aedbb0546dbc4d41a3d9666155d222e9
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2021-07-12 18:26:39 -0600

    Add files via upload

---
+++ Magic 8 Ball/Magic 8 ball V2/Magic_8_ball_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_ball_V2App.swift
+//  Magic 8 ball V2
+//
+//  Created by Kai Azim on 2021-05-07.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_ball_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
